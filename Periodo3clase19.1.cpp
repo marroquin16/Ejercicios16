8cfd756eb7c0a3a9925e534f537114565d0d563d
marroquin16 <112508821+marroquin16@users.noreply.github.com>
2022-09-12 14:30:20 -0600

    Add files via upload

---
+++ Periodo3clase19.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n1,n2, c;
+	cout<<"Ingresar el primer numero:";
+	cin>>n1;
+	cout<<"Ingresar elsegundo numero:";
+	cin>>n2
+	 c=n1;
+	 While (c > n2){
+		cout<< c <<endl;
+		c--;
+	}
+	return 0;
+}
